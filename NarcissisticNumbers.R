b00d08f2e675505e6b383d29a6526e2c757a273d
崔洺溦 <vv@sais-air.home>
2024-06-17 16:13:20 -0400

    Done the rough version, revisit tmr

--- NarcissisticNumbers.R
+++ NarcissisticNumbers.R
@@ -1,11 +1,32 @@
 #Ok so lets start writing some stuff
+#make a plan first
+#ask for a number and check if its numeric
+#break it into digit by keeping it in a string
+#Make the list back into a numeric vector 
+#if number is negative, print message 
+#if number is not threw digits, print a message 
  
 
 number <- readline(prompt = "Enter a Three Digit Positive Number:")
 
-logical.v <- is.numeric(as.numeric(number))
+numeric.form <- as.numeric(number)
 
-if (number < 0){
-  print(paste(number, "is negative, please try again"))
+chr_string <- strsplit(number, split = "")
+
+Numeric_string <- as.numeric(unlist(chr_string))
+
+Sum.cubed.digits <- sum(Numeric_string^3)
+
+
+if (is.na(numeric.form)){
+  print("Input is not a number, please try again.")
+} else if (numeric.form < 0){
+  print(paste(number, "is negative, please try a different number"))
+} else if ((0 < numeric.form & 99 > numeric.form)|numeric.form > 999){
+  print(paste(number, "is not three digits, please try a different number."))
+} else if (Sum.cubed.digits == numeric.form){
+  print(paste(number, "is a Narcissistic Number!"))
+} else {
+  print(paste(number, "is not a Narcissistic Number!"))
 }
 
