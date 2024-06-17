8754456f0df1e072470af7bddae9c86632d5ec94
崔洺溦 <vv@sais-air.home>
2024-06-17 15:11:49 -0400

    3:11 Monday

--- NarcissisticNumbers.R
+++ NarcissisticNumbers.R
@@ -1,1 +1,11 @@
 #Ok so lets start writing some stuff
+ 
+
+number <- readline(prompt = "Enter a Three Digit Positive Number:")
+
+logical.v <- is.numeric(as.numeric(number))
+
+if (number < 0){
+  print(paste(number, "is negative, please try again"))
+}
+
